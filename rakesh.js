3ee62deb1dd3cd6796b2a050d8002c6f6e3ca573
Rakesh Pavan <110276813+rakeshtadi@users.noreply.github.com>
2025-03-11 16:16:28 +0530

    JS Operators

---
+++ rakesh.js
@@ -0,0 +1,19 @@
+//Comparision  operators
+let a = 10;
+let b = 6;
+console.log("a == b" , a==b);
+console.log("a != b", a!=b);
+console.log("a===b", a===b);
+console.log("a!==b", a!==b);
+console.log("a>b", a>b);
+console.log("a < b", a<b);
+console.log("a >= b",a>=b);
+console.log("a <= b",a<=b);
+
+
+//Logical operators
+let cond1 = a>b;
+let cond2 = a==b;
+console.log("cond1 && cond2 = ", cond1&&cond2);
+console.log("cond1 || cond2 =", cond1||cond2);
+console.log("!cond2 =", !cond2);
